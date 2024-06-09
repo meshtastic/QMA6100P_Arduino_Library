04e3bde4fd2ce1dfeafc20810bf13188f56c9dea
Tristan <talderson42@gmail.com>
2024-06-09 00:38:59 -0400

    updated for flashing and testing on altimeter board

--- examples/example1_basic_readings/example1_basic_readings.ino
+++ examples/example1_basic_readings/example1_basic_readings.ino
@@ -1,51 +1,61 @@
 /*example1-BasicReadings*/
 #include <Wire.h>
-#include <QMA6100P.h> // Click here to get the library: http://librarymanager/All# QMA6100P
+#include <Qwiic_QMA6100P.h> // Click here to get the library: http://librarymanager/All# QMA6100P
 
-QMA6100P qmaAccel;
+#define USB_TX_PIN PA12 // D- pin
+#define USB_RX_PIN PA11 // D+ pin
+#define I2C_SDA_PIN PB11
+#define I2C_SCL_PIN PB10
+#define ACCEL_INT1 PB5
+#define ACCEL_INT2 PB6
+#define DB_LED_PIN PA15
+
+bool buffer_enable = false;
+
+QwDevQMA6100P qmaAccel;
 
 outputData myData; // Struct for the accelerometer's data
 
+#include <SoftwareSerial.h>
+
+SoftwareSerial softSerial(USB_RX_PIN, USB_TX_PIN);
+
 void setup()
 {
+  softSerial.begin(38400);
 
+  // put your setup code here, to run once:
+  pinMode(DB_LED_PIN, OUTPUT);
+
+  // Configure I2C
+  Wire.setSDA(I2C_SDA_PIN);
+  Wire.setSCL(I2C_SCL_PIN);
   Wire.begin();
-
-  Serial.begin(115200);
-  Serial.println("Welcome.");
-
-  // Wait for the Serial monitor to be opened.
-  while (!Serial)
-    delay(50);
 
   if (!qmaAccel.begin())
   {
-    Serial.println("Could not communicate with the the QMA6100P. Freezing.");
+    softSerial.println("ERROR: Could not communicate with the the QMA6100P. Freezing.");
     while (1)
       ;
   }
 
-  Serial.println("Ready.");
+  softSerial.println("Ready.");
 
-  if (qmaAccel.softwareReset())
-    Serial.println("Reset.");
+  if (!qmaAccel.softwareReset())
+    softSerial.println("ERROR: Failed to reset");
 
   // Give some time for the accelerometer to reset.
   // It needs two, but give it five for good measure.
   delay(5);
 
-  // Many settings for QMA6100P can only be
-  // applied when the accelerometer is powered down.
-  // However there are many that can be changed "on-the-fly"
-  // check datasheet for more info, or the comments in the
-  // "...regs.h" file which specify which can be changed when.
-  qmaAccel.enableAccel(false);
+  if(!qmaAccel.setRange(SFE_QMA6100P_RANGE32G))        // 32g for the QMA6100P
+    softSerial.println("ERROR: failed to set range");
 
-  qmaAccel.setRange(SFE_QMA6100P_RANGE32G);         // 32g for the QMA6100P
+  if(!qmaAccel.enableDataEngine(false)) // Enables the bit that indicates data is ready.
+    softSerial.println("ERROR: failed to map and set data ready interrupt");
 
-  qmaAccel.enableDataEngine(); // Enables the bit that indicates data is ready.
-  // qmaAccel.setOutputDataRate(); // Default is 50Hz
-  qmaAccel.enableAccel();
+  if(!qmaAccel.enableAccel())
+    softSerial.println("ERROR: failed to set active mode");
 }
 
 void loop()
@@ -53,14 +63,14 @@ void loop()
   // Check if data is ready.
   if (qmaAccel.dataReady())
   {
-    qmaAccel.getAccelData(&myData);
-    Serial.print("X: ");
-    Serial.print(myData.xData, 4);
-    Serial.print(" Y: ");
-    Serial.print(myData.yData, 4);
-    Serial.print(" Z: ");
-    Serial.print(myData.zData, 4);
-    Serial.println();
+    qmaAccel.getAccelData(&myData, buffer_enable);
+    softSerial.print("X: ");
+    softSerial.print(myData.xData, 4);
+    softSerial.print(" Y: ");
+    softSerial.print(myData.yData, 4);
+    softSerial.print(" Z: ");
+    softSerial.print(myData.zData, 4);
+    softSerial.println();
   }
   delay(20); // Delay should be 1/ODR (Output Data Rate), default is 1/50ODR
 }
