60eb79bea6cb813e056bf1de853d167cfac1a73e
Tristan <talderson42@gmail.com>
2024-06-09 00:38:34 -0400

    gutted old i2c bus data type and opted for simpler design all functions are within QwDevQMA6100P

--- Arduino/libraries/QMA6100P/Qwiic_QMA6100P.h
+++ Arduino/libraries/QMA6100P/Qwiic_QMA6100P.h
@@ -10,7 +10,6 @@
 
 #include <Wire.h>
 #include "QMA6100P_regs.h"
-#include "sfe_bus.h"
 
 #define QMA6100P_ADDRESS_HIGH 0x13
 #define QMA6100P_ADDRESS_LOW 0x12
@@ -57,18 +56,14 @@ struct rawOutputData
 class QwDevQMA6100P
 {
 public:
-  QwDevQMA6100P() : _i2cAddress{0}, _cs{0} {};
 
-  int writeRegisterRegion(uint8_t reg, uint8_t *data, uint16_t length);
-  int writeRegisterByte(uint8_t reg, uint8_t data);
-  int readRegisterRegion(uint8_t reg, uint8_t *data, uint16_t length);
-  void setCommunicationBus(sfe_QMA6100P::QwIDeviceBus &theBus, uint8_t i2cAddress);
-
+  bool begin();
   uint8_t getUniqueID();
-  bool initialize(uint8_t settings = DEFAULT_SETTINGS);
+  bool writeRegisterByte(uint8_t registerAddress, uint8_t data);
+  bool readRegisterRegion(uint8_t registerAddress, uint8_t* sensorData, int len);
+  
 
-  bool init(void);
-  bool getAccelData(outputData *userData);
+  bool getAccelData(outputData *userData, bool);
   bool convAccelData(outputData *userAccel, rawOutputData *rawAccelData);
 
   // General Settings
@@ -76,60 +71,11 @@ public:
   bool softwareReset();
   int8_t getOperatingMode();
   bool setRange(uint8_t);
-  bool setInterruptPin(bool enable, uint8_t polarity = 0, uint8_t pulseWidth = 0, bool latchControl = false);
   bool enableDataEngine(bool enable = true);
-  bool setOutputDataRate(uint8_t);
-  float getOutputDataRate();
   bool dataReady();
-  bool runCommandTest();
-  uint8_t readAccelState();
-  bool getRawAccelData(rawOutputData *);
+  bool getRawAccelData(rawOutputData *, bool buffer_enable);
   bool getRawAccelRegisterData(rawOutputData *);
-  bool getRawAccelBufferData(rawOutputData *, int sixteenBit = -1); // Set sixteenBit to 0 to read 8-bit data. Set to 1 to read 16-bit data.
-  float readOutputDataRate();
-
-  // Tap/Double settings
-  bool enableTapEngine(bool enable = true);
-  bool setTapDataRate(uint8_t rate);
-  bool enableDirecTapInterupt(bool enable = true);
-  bool enableDoubleTapInterrupt(bool enable = true);
-
-  // Tilt Settings
-  bool enableTiltEngine(bool enable = true);
-  bool setTiltDataRate(uint8_t rate);
-
-  // Wake/Sleep Settings
-  bool setWakeDataRate(uint8_t rate);
-  bool enableSleepEngine(bool enable = true);
-  bool enableWakeEngine(bool enable = true);
-  bool forceWake();
-  bool forceSleep();
-
-  // Interrupt Settings
-  bool configureInterruptPin(uint8_t pinVal);
-  bool routeHardwareInterrupt(uint8_t, uint8_t pin = 1);
-  bool enablePhysInterrupt(bool enable = true, uint8_t pin = 1);
-  bool setPinMode(bool activeHigh = true, uint8_t pin = 1);
-  bool setLatchControl(bool pulsed = true, uint8_t pin = 1);
-  bool setPulseWidth(uint8_t width, uint8_t pin = 1);
-  bool clearInterrupt();
-  bool tapDetected();
-  int8_t getDirection();
-  bool unknownTap();
-  bool waterMarkReached();
-  bool bufferFull();
-  bool freeFall();
-  bool doubleTapDetected();
-  bool tiltChange();
-
-  // Buffer Setttings
-  bool setBufferThreshold(uint8_t);
-  bool setBufferOperationMode(uint8_t operationMode);
-  bool setBufferResolution(bool sixteenBit = true);
-  bool enableBufferInt(bool enable = true);
-  bool enableSampleBuffer(bool enable = true);
-  uint16_t getSampleLevel();
-  bool clearBuffer();
+  bool getRawAccelBufferData(rawOutputData *rawAccelData, int sixteenBit);
 
   rawOutputData rawAccelData;
 
@@ -141,8 +87,5 @@ public:
   const double convRange32G = .003910;
 
 protected:
-  sfe_QMA6100P::QwIDeviceBus *_sfeBus;
-  uint8_t _i2cAddress;
-  uint8_t _cs;
   int _range = -1; // Keep a local copy of the range. Default to "unknown" (-1).
 };
